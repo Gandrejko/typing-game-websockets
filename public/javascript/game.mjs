1e69841500411c4b48ddb7325c5782b536ef88b5
Andrew <gandrejko2002@gmail.com>
2023-02-18 21:48:13 +0200

    add ability to leave room

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -24,10 +24,11 @@ const onSubmit = (inputModal) => {
 	socket.emit("ADD_ROOM", roomName);
 }
 
-const updateRooms = ({roomName, numberOfUsersDefault}) => {
-	if(roomName) {
-		appendRoomElement({name: roomName, numberOfUsers: numberOfUsersDefault})
+const updateRooms = ({roomName, numberOfUsers}) => {
+	if(!roomName) {
+		return;
 	}
+	appendRoomElement({name: roomName, numberOfUsers: numberOfUsers})
 }
 
 const joinRoomDone = (roomName) => {
@@ -41,6 +42,23 @@ const joinRoomDone = (roomName) => {
 	roomTitle.innerText = roomName;
 }
 
+const leaveRoom = () => {
+	const roomName = document.getElementById('room-name');
+	socket.emit("LEAVE_ROOM", roomName.innerText)
+}
+
+const quitRoomBtn = document.getElementById('quit-room-btn');
+quitRoomBtn.addEventListener('click', leaveRoom);
+
+const leaveRoomDone = () => {
+	const gamePage = document.getElementById('game-page');
+	const roomsPage = document.getElementById('rooms-page');
+
+	addClass(gamePage, 'display-none');
+	removeClass(roomsPage, 'display-none');
+}
+
+socket.on("LEAVE_ROOM_DONE", leaveRoomDone);
 socket.on("JOIN_ROOM_DONE", joinRoomDone);
 socket.on("UPDATE_ROOMS", updateRooms);
 
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,23 +3,31 @@ import { Server } from 'socket.io';
 const rooms: string[] = [];
 const roomsMap = new Map(rooms.map(roomName => [roomName, 0]));
 const numberOfUsersDefault = 0;
-
+console.log(roomsMap)
 export default (io: Server) => {
 	io.on('connection', socket => {
 		const username = socket.handshake.query.username;
 		roomsMap.forEach((numberOfUsers, roomName, map) => {
-			socket.emit("UPDATE_ROOMS", {roomName, numberOfUsersDefault});
+			if(numberOfUsers > 0) {
+				socket.emit("UPDATE_ROOMS", {roomName, numberOfUsers});
+			}
 		})
 
 		socket.on("ADD_ROOM", (roomName: string) => {
+			const numberOfUsers = numberOfUsersDefault + 1;
 			rooms.push(roomName);
-			roomsMap.set(roomName, numberOfUsersDefault);
+			roomsMap.set(roomName, numberOfUsers);
 
-			socket.emit("UPDATE_ROOMS", {roomName, numberOfUsersDefault});
-			socket.broadcast.emit("UPDATE_ROOMS", {roomName, numberOfUsersDefault});
+			socket.emit("UPDATE_ROOMS", {roomName, numberOfUsers});
+			socket.broadcast.emit("UPDATE_ROOMS", {roomName, numberOfUsers});
 
 			socket.join(roomName);
 			io.to(socket.id).emit("JOIN_ROOM_DONE", roomName);
+		})
+
+		socket.on("LEAVE_ROOM", (roomName: string) => {
+			socket.leave(roomName);
+			io.to(socket.id).emit("LEAVE_ROOM_DONE", roomName);
 		})
 	});
 };
